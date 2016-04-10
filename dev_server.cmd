f64651a461be08c009bb9c1d531d74ad80d6c7a7
Joe Collins <joe_collins@blackradley.com>
2016-04-10 20:51:43 +0100

    Dev server for Windows

--- dev_server.cmd
+++ dev_server.cmd
@@ -1,4 +1,4 @@
 :: Run up the GAE development server
-c:\python27\python "c:\Program Files\Google\google_appengine\dev_appserver.py" ./web_app/ --host=0.0.0.0
+c:\python27\python "C:\Program Files (x86)\Google\google_appengine\dev_appserver.py" ./web_app/ --host=0.0.0.0
 pause
  
