686ec7473c20e82f79aa48b6401b79d742b8ceb6
Joe J Collins <joejcollins@gmail.com>
2018-02-13 19:09:21 +0000

    Linux set upt

--- dev_server.sh
+++ dev_server.sh
@@ -1,2 +1,2 @@
 # Run up the GAE development server
-python ../google_appengine/dev_appserver.py ./web_app/ --host=0.0.0.0  
+python ..//google-cloud-sdk/platform/google_appengine/dev_appserver.py ./web_app/ --host=0.0.0.0  
